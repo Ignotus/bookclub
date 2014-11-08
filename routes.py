e6505e3ad4764667ea05e5ecb7f8236a02e945bf
Minh Ngo <nlminhtl@gmail.com>
2014-11-08 03:14:16 +0200

    Secure password :)

--- routes.py
+++ routes.py
@@ -14,9 +14,8 @@ def main():
         return redirect(url_for('home'))
 
     next_url = request.args.get('next') or url_for('home')
-    return facebook.authorize(callback=url_for('facebook_authorized',
-                                               next=next_url,
-                                               _external=True))
+
+    return render_template('authorization.html', next=next_url)
 
 
 @app.route('/progress/update', methods=['POST'])
@@ -189,14 +188,27 @@ def home():
                            last_progress_info=last_progress_info)
 
 
+@app.route('/login', methods=["POST"])
+def login():
+    next_url = request.form['next']
+
+    if 'password' in request.form and request.form['password'] == PASSWORD:
+        return facebook.authorize(callback=url_for('facebook_authorized',
+                                  next=next_url,
+                                  _external=True))
+    else:
+        return "Where are you from, dude? Your password isn't correct"
+
 @app.route('/login/authorized')
 @facebook.authorized_handler
 def facebook_authorized(resp):
     next_url = request.args.get('next') or url_for('home')
+
     if resp is None:
         # The user likely denied the request
         flash(u'There was a problem logging in.')
         return redirect(next_url)
+
     session['oauth_token'] = (resp['access_token'], '')
     user_data = facebook.get('/me').data
     user = User.query.filter(User.email == user_data['email']).first()
@@ -207,4 +219,5 @@ def facebook_authorized(resp):
         login_user(new_user)
     else:
         login_user(user)
+
     return redirect(next_url)
