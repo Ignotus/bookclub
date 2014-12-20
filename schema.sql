47e22b1bfa1e1c22a530c6076bab87824bdd9569
Minh Ngo <nlminhtl@gmail.com>
2014-12-20 23:44:10 +0000

    Port schema.sql for PostgreSQL

--- schema.sql
+++ schema.sql
@@ -1,44 +1,36 @@
-create table users(id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                   email VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci,
-                   first_name VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-                   last_name VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+create table users(id serial not null primary key,
+                   email varchar(50),
+                   first_name varchar(50) not null,
+                   last_name varchar(50) not null
                   );
 
-create table books(id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                   book_author VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-                   book_name VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci UNIQUE NOT NULL,
-                   img TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-                   description TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-                   url VARCHAR(300) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-                   comment_count INTEGER DEFAULT 0);
+create table books(id serial not null primary key,
+                   book_author varchar(100) not null,
+                   book_name varchar(100) not null,
+                   img text not null,
+                   description text not null,
+                   url varchar(300) not null,
+                   comment_count integer default 0
+                  );
 
-create table common(id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                    `key` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci UNIQUE NOT NULL,
-                    value VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL);
+create table common(id serial not null primary key,
+                    "key" varchar(50) not null,
+                    value varchar(50) not null
+                   );
 
-create table progress(id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                      timestamp DATETIME NOT NULL,
-                      book_id INTEGER NOT NULL,
-                      user_id INTEGER NOT NULL,
-                      progress INTEGER NOT NULL,
-                      FOREIGN KEY (book_id) REFERENCES books(id)
-                      ON DELETE CASCADE
-                      ON UPDATE CASCADE,
-                      FOREIGN KEY (user_id) REFERENCES users(id)
-                      ON DELETE CASCADE
-                      ON UPDATE CASCADE);
+create table progress(id serial not null primary key,
+                      timestamp timestamp not null,
+                      book_id integer not null references books on delete cascade on update cascade,
+                      user_id integer not null references users on delete cascade on update cascade,
+                      progress integer not null
+                     );
 
-create table comments(id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                      timestamp DATETIME NOT NULL,
-                      book_id INTEGER NOT NULL,
-                      user_id INTEGER NOT NULL,
-                      comment TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-                      FOREIGN KEY (book_id) REFERENCES books(id)
-                      ON DELETE CASCADE
-                      ON UPDATE CASCADE,
-                      FOREIGN KEY (user_id) REFERENCES users(id)
-                      ON DELETE CASCADE
-                      ON UPDATE CASCADE);
+create table comments(id serial not null primary key,
+                      timestamp timestamp not null,
+                      book_id integer not null references books on delete cascade on update cascade,
+                      user_id integer not null references users on delete cascade on update cascade,
+                      comment text not null
+                     );
 
 create view comments_detailed(id,
                               timestamp,
@@ -47,19 +39,17 @@ create view comments_detailed(id,
                               user_last_name,
                               comment
                               )
-  AS SELECT comments.id, comments.timestamp, comments.book_id,
-            users.first_name, users.last_name, comments.comment FROM comments INNER JOIN users ON comments.user_id=users.id;
+  as select comments.id, comments.timestamp, comments.book_id,
+            users.first_name, users.last_name, comments.comment from comments inner join users on comments.user_id=users.id;
 
-create table blog(id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                  timestamp DATETIME NOT NULL,
-                  last_update DATETIME NOT NULL,
-                  user_id INTEGER NOT NULL,
-                  topic TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL, 
-                  content TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-                  tags TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-                  FOREIGN KEY (user_id) REFERENCES users(id)
-                  ON DELETE CASCADE
-                  ON UPDATE CASCADE);
+create table blog(id serial not null primary key,
+                  timestamp timestamp not null,
+                  last_update timestamp not null,
+                  user_id integer not null references users on delete cascade on update cascade,
+                  topic text not null,
+                  content text not null,
+                  tags text not null
+                 );
 
 create view blog_detailed(id,
                           timestamp,
@@ -70,13 +60,11 @@ create view blog_detailed(id,
                           content,
                           tags
                           )
-  AS SELECT blog.id, blog.timestamp, blog.last_update,
+  as select blog.id, blog.timestamp, blog.last_update,
             users.first_name, users.last_name,
-            blog.topic, blog.content, blog.tags FROM blog INNER JOIN users ON blog.user_id=users.id;
+            blog.topic, blog.content, blog.tags from blog inner join users on blog.user_id=users.id;
 
-create table tags(id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                  blog_id INTEGER NOT NULL,
-                  tag VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-                  FOREIGN KEY (blog_id) REFERENCES blog(id)
-                  ON DELETE CASCADE
-                  ON UPDATE CASCADE);
+create table tags(id serial not null primary key,
+                  blog_id integer not null references blog on delete cascade on update cascade,
+                  tag varchar(50) not null
+                 );
